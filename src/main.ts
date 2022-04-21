eabe7118bd9f4293f17f3bc036f5da23c20ef319
tomguillermou <tomguillermou@gmail.com>
2022-04-21 03:01:58 +0200

    📢 Add request logging

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
 
@@ -8,6 +9,7 @@ async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
 
     app.useGlobalPipes(new ValidationPipe());
+    app.use(morgan('tiny'));
 
     const port = app.get(ConfigService).get<string>('PORT');
 
